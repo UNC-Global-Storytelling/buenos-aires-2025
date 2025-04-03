e2596a0d48ae14ed1e7411b3f43b3d1480c635af
anna <aefetter@unc.edu>
2025-04-02 20:29:40 -0400

    styling for youtube! (shortcodes)

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,8 @@ import cssnano from 'cssnano';
 import postcss from 'postcss';
 import tailwindcss from '@tailwindcss/postcss';
 import markdownIt from "markdown-it";
+import video from "./src/_includes/components/shortcodes/video.js";
+import {EleventyRenderPlugin} from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
   // Compile Tailwind before Eleventy processes the files
@@ -59,6 +61,13 @@ export default function (eleventyConfig) {
   eleventyConfig.addCollection("stories", function (collectionApi) {
     return collectionApi.getFilteredByGlob("./src/stories/*.md");
   });
+
+  // Plug in sections
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+
+
+  // Add shortcodes
+  eleventyConfig.addShortcode("video", video);
 
   return {
     dir: {
---
+++ src/stories/annas-story.md
@@ -0,0 +1,27 @@
+---
+layout: layouts/story
+title: Anna's Story
+reporter_byline: Anna
+video_1_byline: Anna
+video_2_byline: Anna
+photo_byline: Anna
+interactive_1_byline: Anna
+interactive_2_byline: Anna
+uca_contributor_1_byline: Anna
+uca_contributor_2_byline: Anna
+body_copy: >-
+  ## subheading
+
+
+  Hi this is my *styling* **TEST** !
+
+
+  > "Here's my pretty pull quote."
+
+
+  And here's a link to [Anna's blog](https://globalstorytelling.unc.edu/cutaway/terminal-to-terminal/).
+
+
+  {% video { "videoId": "768107556" } %}
+image: /assets/img/uploads/img_1893.jpg
+---
