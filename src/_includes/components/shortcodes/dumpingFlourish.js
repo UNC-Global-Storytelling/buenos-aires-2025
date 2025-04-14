c119b120c5da4da961c75eab3db4dcb12a74ccf3
emilyr0 <emroll@unc.edu>
2025-04-14 11:20:50 -0400

    I added two new shortcodes to the project: dumpingFlourish and timelineBueno. The flourish works and the timeline is in progress.

---
+++ src/_includes/components/shortcodes/dumpingFlourish.js
@@ -0,0 +1,13 @@
+export default function () {
+  return `
+    <div style="width: 80%; height: auto; overflow: hidden; margin: 0 auto;">
+      <div class="flourish-embed flourish-map" data-src="visualisation/22573429" style="width: 100%; height: 100%;">
+        <script src="https://public.flourish.studio/resources/embed.js"></script>
+        <noscript>
+          <img src="https://public.flourish.studio/visualisation/22573429/thumbnail" width="100%" alt="map visualization" />
+        </noscript>
+      </div>
+    </div>
+  `;
+}
+//this is the flourish component for open air dumping
