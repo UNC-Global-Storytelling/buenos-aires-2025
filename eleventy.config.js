609bbdb8fad2f86bddfcb3714be49ec070d09ce3
emilyr0 <emroll@unc.edu>
2025-04-14 14:30:06 -0400

    Added interactive for Gig Economy

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,6 +9,7 @@ import video from "./src/_includes/components/shortcodes/video.js";
 import timeline from "./src/_includes/components/shortcodes/timelineBueno.js";
 import dumpingFlourish from "./src/_includes/components/shortcodes/dumpingFlourish.js";
 import separationFlourish from "./src/_includes/components/shortcodes/separationFlourish.js";
+import gigGenially from "./src/_includes/components/shortcodes/gigGenially.js";
 import {EleventyRenderPlugin} from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
@@ -75,6 +76,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addShortcode("timeline", timeline);
   eleventyConfig.addShortcode("dumpingFlourish", dumpingFlourish);
   eleventyConfig.addShortcode("separationFlourish", separationFlourish);
+  eleventyConfig.addShortcode("gigGenially", gigGenially);
 
   return {
     dir: {
