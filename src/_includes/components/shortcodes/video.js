401d64a1952691dbd14f696c144c67af6de84755
emilyr0 <emroll@unc.edu>
2025-04-21 19:45:45 -0400

    changed many styles

--- src/_includes/components/shortcodes/video.js
+++ src/_includes/components/shortcodes/video.js
@@ -1,26 +1,24 @@
 export default (data) => {
-    const { videoId } = data;
-    return `
-      <div class="video-container">
-        <div class="video-player">
-          <iframe 
-            src="https://player.vimeo.com/video/${videoId}" 
-            frameborder="0" 
-            allow="autoplay; fullscreen; picture-in-picture" 
-            allowfullscreen>
-          </iframe>
-        </div>
-  
-        <button 
-          id="toggleOverlayBtn" 
-          class="rounded-full bg-[var(--color-green)] text-white font-medium px-6 py-2 mt-4 transition hover:bg-[var(--color-brown)]">
-          Lights Out Display
-        </button>
+  const { videoId } = data;
+  return `
+    <div class="video-container">
+      <div class="video-player">
+        <iframe 
+          src="https://player.vimeo.com/video/${videoId}" 
+          frameborder="0" 
+          allow="autoplay; fullscreen; picture-in-picture" 
+          allowfullscreen>
+        </iframe>
       </div>
-  
-      <div class="overlay" id="videoOverlay"></div>
-  
-      <script src="https://player.vimeo.com/api/player.js"></script>
-    `;
-  }
-  
+
+      <button 
+        id="toggleOverlayBtn" 
+        class="rounded-full bg-[var(--color-green)] text-white font-medium px-6 py-2 mt-4 transition hover:bg-[var(--color-brown)]">
+        Lights Out Display
+      </button>
+
+      <div class="overlay fixed inset-0 bg-black opacity-0 pointer-events-none transition-opacity duration-300 z-50" id="videoOverlay"></div>
+    </div>
+    
+  `;
+};
