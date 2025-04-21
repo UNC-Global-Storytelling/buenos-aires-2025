ed37bf2e9952b475956e6510a891e88923e76ff6
anna <aefetter@unc.edu>
2025-04-21 19:37:18 -0400

    update pull quote format to not require attribution

--- src/_includes/components/shortcodes/pullQuote.js
+++ src/_includes/components/shortcodes/pullQuote.js
@@ -1,6 +1,17 @@
 export default function(data) {
-  const { quote, attribution, role } = data;
-  
-  // Remove extra whitespace by using a single line
-  return `<div class="pull-quote"><blockquote><p>"${quote}"</p><footer><span class="attribution">${attribution}</span>${role ? `<span class="role">${role}</span>` : ''}</footer></blockquote></div>`;
+  const { quote, attribution = '', role = '' } = data;
+
+  return `
+    <div class="pull-quote">
+      <blockquote>
+        <p>"${quote}"</p>
+        ${attribution || role ? `
+          <footer>
+            ${attribution ? `<span class="attribution">${attribution}</span>` : ''}
+            ${role ? `<span class="role">${role}</span>` : ''}
+          </footer>
+        ` : ''}
+      </blockquote>
+    </div>
+  `;
 }
